5aa846ac4cdce30a6de8db1850a29b528919db34
ardysyahputraa <ardysyahputra01@gmail.com>
2022-06-30 15:09:32 +0700

    Update Request.kt

--- sdk/src/main/java/com/akggames/sdk/Request.kt
+++ sdk/src/main/java/com/akggames/sdk/Request.kt
@@ -13,10 +13,10 @@ import retrofit2.Response
 object Request {
 
 
-    open fun send_otp(context: Context,email: String?) {
+    open fun send_otp(context: Context,email: String) {
         var apiInterface: ApiService = ApiClient.getClient(context).create(ApiService::class.java)
 
-        apiInterface.sendOTP("ardysyahputra01@gmail.com").enqueue(object : Callback<ResponseBody>{
+        apiInterface.sendOTP(email).enqueue(object : Callback<ResponseBody>{
             override fun onResponse(call: Call<ResponseBody>, response: Response<ResponseBody>) {
                 Log.d("AKGGAMESSDK","Response : "+response.body())
             }
